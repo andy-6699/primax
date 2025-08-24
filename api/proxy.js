b9981c1952f61095d5bf67d8faefc4c8224d997f
andy-6699 <nihaosao6699@gmail.com>
2025-08-24 17:56:01 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  const targetUrl = 'https://bpl.ptpica.cc/ph';
+  const targetUrl = 'https://primax.hzjinfengwj.com/ec';
   
   // 直接返回纯净的iframe页面，无任何多余文字
   res.setHeader('Content-Type', 'text/html');
