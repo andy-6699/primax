9b24d0863ef50b2fd4ff1bec5b6046e02f578758
andy-6699 <nihaosao6699@gmail.com>
2025-08-24 10:01:47 +0800

    feat: clean iframe without any extra text

--- api/proxy.js
+++ api/proxy.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  const targetUrl = 'https://xb6rf.com/ph';
+  const targetUrl = 'https://bpl.ptpica.cc/ph';
   
   // 直接返回纯净的iframe页面，无任何多余文字
   res.setHeader('Content-Type', 'text/html');
